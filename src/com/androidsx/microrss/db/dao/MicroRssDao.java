29722671041c627071487956e31ab7ce92cf8e60
Pablo Pera <pablo.sx@gmail.com>
2011-07-03 18:10:42 +0000

    [microrss] Use a content resolver, for normal cursors, instead of the activity with its managed cursors for the DAO, since we'll update from the service

--- src/com/androidsx/microrss/db/dao/MicroRssDao.java
+++ src/com/androidsx/microrss/db/dao/MicroRssDao.java
@@ -4,7 +4,7 @@ import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
-import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.ContentUris;
 import android.database.Cursor;
 import android.net.Uri;
@@ -16,11 +16,10 @@ import com.androidsx.microrss.domain.DefaultItem;
 import com.androidsx.microrss.domain.Item;
 
 public class MicroRssDao {
+    private final ContentResolver contentResolver;
 
-    private final Activity activity;
-
-    public MicroRssDao(Activity activity) {
-        this.activity = activity;
+    public MicroRssDao(ContentResolver contentResolver) {
+        this.contentResolver = contentResolver;
     }
 
     protected void persistFeed() {
@@ -40,48 +39,69 @@ public class MicroRssDao {
     }
 
     public List<Item> findStories(int feedId) throws DataNotFoundException {
-        final Uri aFeedUri = ContentUris.withAppendedId(MicroRssContentProvider.FEEDS_CONTENT_URI, feedId);
-        final Uri allStoriesUriInFeedUri = Uri.withAppendedPath(aFeedUri, MicroRssContentProvider.TABLE_ITEMS);
-        final String[] projection = new String[] { ItemColumns.TITLE, ItemColumns.CONTENT,
-                ItemColumns.ITEM_URL, ItemColumns.DATE };
-        final Cursor cursor = activity.managedQuery(allStoriesUriInFeedUri, projection, null, null,
-                ItemColumns.POSITION + " DESC");
-
-        List<Item> items = new LinkedList<Item>();
-        if (cursor != null && cursor.moveToFirst()) {
-            do {
-                items.add(itemFromCursor(cursor));
-            } while (cursor.moveToNext());
+        Cursor cursor = null;
+        try {
+            final Uri aFeedUri = ContentUris.withAppendedId(MicroRssContentProvider.FEEDS_CONTENT_URI, feedId);
+            final Uri allStoriesUriInFeedUri = Uri.withAppendedPath(aFeedUri, MicroRssContentProvider.TABLE_ITEMS);
+            final String[] projection = new String[] { ItemColumns.TITLE, ItemColumns.CONTENT,
+                    ItemColumns.ITEM_URL, ItemColumns.DATE };
+            cursor = contentResolver.query(allStoriesUriInFeedUri, projection, null, null,
+                    ItemColumns.POSITION + " DESC");
+    
+            List<Item> items = new LinkedList<Item>();
+            if (cursor != null && cursor.moveToFirst()) {
+                do {
+                    items.add(itemFromCursor(cursor));
+                } while (cursor.moveToNext());
+            }
+            return items;
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
         }
-        return items;
     }
     
     /** @return sorted IDs of all the stories in a feed, given its ID */
     public int[] findStoryIds(int feedId) throws DataNotFoundException {
-        final Uri aFeedUri = ContentUris.withAppendedId(MicroRssContentProvider.FEEDS_CONTENT_URI, feedId);
-        final Uri allStoriesUriInFeedUri = Uri.withAppendedPath(aFeedUri, MicroRssContentProvider.TABLE_ITEMS);
-        final String[] projection = new String[] { BaseColumns._ID };
-        final Cursor cursor = activity.managedQuery(allStoriesUriInFeedUri, projection, null, null,
-                ItemColumns.POSITION + " DESC");
-
-        List<Integer> ids = new LinkedList<Integer>();
-        if (cursor != null && cursor.moveToFirst()) {
-            do {
-                ids.add(cursor.getInt(cursor.getColumnIndex(BaseColumns._ID)));
-            } while (cursor.moveToNext());
+        Cursor cursor = null;
+        try {
+            final Uri aFeedUri = ContentUris.withAppendedId(MicroRssContentProvider.FEEDS_CONTENT_URI, feedId);
+            final Uri allStoriesUriInFeedUri = Uri.withAppendedPath(aFeedUri, MicroRssContentProvider.TABLE_ITEMS);
+            final String[] projection = new String[] { BaseColumns._ID };
+            cursor = contentResolver.query(allStoriesUriInFeedUri, projection, null, null,
+                    ItemColumns.POSITION + " DESC");
+    
+            List<Integer> ids = new LinkedList<Integer>();
+            if (cursor != null && cursor.moveToFirst()) {
+                do {
+                    ids.add(cursor.getInt(cursor.getColumnIndex(BaseColumns._ID)));
+                } while (cursor.moveToNext());
+            }
+            return toIntArray(ids);
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
         }
-        return toIntArray(ids);
     }
 
     /** @return a single story, given its ID */
     public Item findStory(int id) throws DataNotFoundException {
-        final Uri anItemUri = ContentUris.withAppendedId(MicroRssContentProvider.ITEMS_CONTENT_URI, id);
-        final String[] projection = new String[] { ItemColumns.TITLE, ItemColumns.CONTENT, ItemColumns.ITEM_URL, ItemColumns.DATE };
-        final Cursor cursor = activity.managedQuery(anItemUri, projection, null, null, null);
-        if (cursor != null && cursor.moveToFirst()) {
-            return itemFromCursor(cursor);
-        } else {
-            throw new DataNotFoundException("No story was found for the id " + id);
+        Cursor cursor = null;
+        try {
+            final Uri anItemUri = ContentUris.withAppendedId(MicroRssContentProvider.ITEMS_CONTENT_URI, id);
+            final String[] projection = new String[] { ItemColumns.TITLE, ItemColumns.CONTENT, ItemColumns.ITEM_URL, ItemColumns.DATE };
+            cursor = contentResolver.query(anItemUri, projection, null, null, null);
+            if (cursor != null && cursor.moveToFirst()) {
+                return itemFromCursor(cursor);
+            } else {
+                throw new DataNotFoundException("No story was found for the id " + id);
+            }
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
         }
     }
 
