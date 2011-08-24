f79f416f9734a2744b97673a57997701fadad5e2
Pablo Pera <pablo.sx@gmail.com>
2011-08-24 11:50:15 +0000

    [microrss] Trato de corregir NPE con el click del header del story activity. Si el parent es null, sencillamente no haremos nada. Es raro, en mi movil el padre nunca es null..., re #264

--- src/com/androidsx/commons/helper/ComponentHelper.java
+++ src/com/androidsx/commons/helper/ComponentHelper.java
@@ -1,6 +1,7 @@
 package com.androidsx.commons.helper;
 
 import android.graphics.Rect;
+import android.util.Log;
 import android.view.TouchDelegate;
 import android.view.View;
 
@@ -8,24 +9,28 @@ import android.view.View;
  * Helper and utility methods for the UI components.
  */
 public class ComponentHelper {
-    
-    public static void increaseTouchArea(final View view, View parent, final int pixels) {
-        parent.post(new Runnable() {
-            @Override
-            public void run() {
-                Rect delegateArea = new Rect();
-                View delegate = view;
-                delegate.getHitRect(delegateArea);
-                delegateArea.left -= pixels;
-                delegateArea.right += pixels;
-                delegateArea.top -= pixels;
-                delegateArea.bottom += pixels;
-                TouchDelegate expandedArea = new TouchDelegate(delegateArea, delegate);
-                if (View.class.isInstance(delegate.getParent())) {
-                    ((View) delegate.getParent()).setTouchDelegate(expandedArea);
-                }
-            }
-        });
+    private static final String TAG = "ComponentHelper";
 
+    public static void increaseTouchArea(final View view, View parent, final int pixels) {
+        if (parent == null) {
+            Log.w(TAG, "The parent for the exit button is null: can't expand the touch area");
+        } else {
+            parent.post(new Runnable() {
+                @Override
+                public void run() {
+                    Rect delegateArea = new Rect();
+                    View delegate = view;
+                    delegate.getHitRect(delegateArea);
+                    delegateArea.left -= pixels;
+                    delegateArea.right += pixels;
+                    delegateArea.top -= pixels;
+                    delegateArea.bottom += pixels;
+                    TouchDelegate expandedArea = new TouchDelegate(delegateArea, delegate);
+                    if (View.class.isInstance(delegate.getParent())) {
+                        ((View) delegate.getParent()).setTouchDelegate(expandedArea);
+                    }
+                }
+            });
+        }
     }
 }
