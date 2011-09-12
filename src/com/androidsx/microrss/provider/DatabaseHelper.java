cfccd22643ec0902b35d47623a70c0e589c52a73
Omar Pera <campbell.sx@gmail.com>
2011-09-12 18:04:32 +0000

    [microrss] Remove some feeds and put the definitive active ones.

--- src/com/androidsx/microrss/provider/DatabaseHelper.java
+++ src/com/androidsx/microrss/provider/DatabaseHelper.java
@@ -67,10 +67,10 @@ class DatabaseHelper extends SQLiteOpenHelper {
 	        		+ " VALUES (?)";
 			SQLiteStatement insert = db.compileStatement(sql);
 
-			insert.bindString(1, "News");
+			insert.bindString(1, "Technology");
 			insert.executeInsert();
 			
-			insert.bindString(1, "Technology");
+			insert.bindString(1, "News");
 			insert.executeInsert();
 			
 			insert.bindString(1, "Sports");
@@ -107,125 +107,107 @@ class DatabaseHelper extends SQLiteOpenHelper {
 	        		 + " VALUES (?, ?, ?, ?, -1, 0)";
 			SQLiteStatement insert = db.compileStatement(sql);
 			
-			// 1 - NEWS
-			insert.bindLong(1, 1);
-			insert.bindString(2,
-					"http://news.google.com/news?ned=us&topic=h&output=rss");
-			insert.bindString(3, "Google News");
-			insert.bindLong(4, 1);
-			insert.executeInsert();
+			// 1 - TECH
 
 			insert.bindLong(1, 1);
-			insert.bindString(2, "http://feeds.bbci.co.uk/news/rss.xml");
-			insert.bindString(3, "BBC Top Stories");
-			insert.bindLong(4, 1);
-			insert.executeInsert();
-
-			insert.bindLong(1, 1);
-			insert.bindString(2,
-					"http://www.nytimes.com/services/xml/rss/nyt/GlobalHome.xml");
-			insert.bindString(3, "NYTimes World");
-			insert.bindLong(4, 0);
-			insert.executeInsert();
-
-			insert.bindLong(1, 1);
-			insert.bindString(2,
-					"http://news.bbc.co.uk/rss/newsonline_world_edition/front_page/rss.xml");
-			insert.bindString(3, "BBC World");
-			insert.bindLong(4, 0);
-			insert.executeInsert();
-
-			insert.bindLong(1, 1);
-			insert.bindString(2, "http://rss.cnn.com/rss/edition.rss");
-			insert.bindString(3, "CNN Top Stories");
-			insert.bindLong(4, 0);
-			insert.executeInsert();
-
-			insert.bindLong(1, 1);
-			insert.bindString(2,
-					"http://www.foxnews.com/xmlfeed/rss/0,4313,0,00.rss");
-			insert.bindString(3, "Fox News");
-			insert.bindLong(4, 0);
-			insert.executeInsert();
-
-			insert.bindLong(1, 1);
-			insert.bindString(2, "http://rss.news.yahoo.com/rss/topstories");
-			insert.bindString(3, "Yahoo News");
-			insert.bindLong(4, 1);
-			insert.executeInsert();
-
-			// 2 - TECH
-
-			insert.bindLong(1, 2);
 			insert.bindString(2, "http://feeds.feedburner.com/Techcrunch");
 			insert.bindString(3, "TechCrunch");
 			insert.bindLong(4, 1);
 			insert.executeInsert();
 
-			insert.bindLong(1, 2);
+			insert.bindLong(1, 1);
 			insert.bindString(2, "http://www.engadget.com/rss.xml");
 			insert.bindString(3, "Engadget");
 			insert.bindLong(4, 1);
 			insert.executeInsert();
 
-			insert.bindLong(1, 2);
+			insert.bindLong(1, 1);
 			insert.bindString(2,
 					"http://googleblog.blogspot.com/feeds/posts/default?alt=rss");
 			insert.bindString(3, "The Official Google Blog");
 			insert.bindLong(4, 0);
 			insert.executeInsert();
 
-			insert.bindLong(1, 2);
-			insert.bindString(2, "http://feeds.mashable.com/mashable");
-			insert.bindString(3, "Mashable!");
-			insert.bindLong(4, 0);
-			insert.executeInsert();
-
-			insert.bindLong(1, 2);
+			insert.bindLong(1, 1);
 			insert.bindString(2, "http://www.bgr.com/feed/");
 			insert.bindString(3, "BGR");
 			insert.bindLong(4, 0);
 			insert.executeInsert();
 
-			insert.bindLong(1, 2);
+			insert.bindLong(1, 1);
 			insert.bindString(2, "http://feeds2.feedburner.com/thenextweb");
 			insert.bindString(3, "The Next Web");
 			insert.bindLong(4, 0);
 			insert.executeInsert();
 
-			insert.bindLong(1, 2);
-			insert.bindString(2, "http://feeds.slashgear.com/slashgear");
-			insert.bindString(3, "SlashGear");
-			insert.bindLong(4, 0);
-			insert.executeInsert();
-
-			insert.bindLong(1, 2);
+			insert.bindLong(1, 1);
 			insert.bindString(2, "http://feeds.feedburner.com/ommalik");
 			insert.bindString(3, "GigaOM");
 			insert.bindLong(4, 0);
 			insert.executeInsert();
 
-			insert.bindLong(1, 2);
+			insert.bindLong(1, 1);
 			insert.bindString(2, "http://rss.slashdot.org/slashdot/eqWf");
 			insert.bindString(3, "Slashdot");
 			insert.bindLong(4, 0);
 			insert.executeInsert();
 
-			insert.bindLong(1, 2);
+			insert.bindLong(1, 1);
 			insert.bindString(2, "http://lifehacker.com/index.xml");
 			insert.bindString(3, "LifeHacker");
 			insert.bindLong(4, 0);
 			insert.executeInsert();
 
-			insert.bindLong(1, 2);
+			insert.bindLong(1, 1);
 			insert.bindString(2, "http://www.readwriteweb.com/rss.xml");
 			insert.bindString(3, "ReadWriteWeb");
 			insert.bindLong(4, 1);
 			insert.executeInsert();
 
-			insert.bindLong(1, 2);
+			insert.bindLong(1, 1);
 			insert.bindString(2, "http://www.gizmodo.net/index.xml");
 			insert.bindString(3, "Gizmodo");
+			insert.bindLong(4, 0);
+			insert.executeInsert();
+
+			
+			// 2 - NEWS
+			insert.bindLong(1, 2);
+			insert.bindString(2,
+					"http://news.google.com/news?ned=us&topic=h&output=rss");
+			insert.bindString(3, "Google News");
+			insert.bindLong(4, 1);
+			insert.executeInsert();
+
+			insert.bindLong(1, 2);
+			insert.bindString(2, "http://feeds.bbci.co.uk/news/rss.xml");
+			insert.bindString(3, "BBC Top Stories");
+			insert.bindLong(4, 1);
+			insert.executeInsert();
+
+			insert.bindLong(1, 2);
+			insert.bindString(2,
+					"http://www.nytimes.com/services/xml/rss/nyt/GlobalHome.xml");
+			insert.bindString(3, "NYTimes World");
+			insert.bindLong(4, 0);
+			insert.executeInsert();
+
+			insert.bindLong(1, 2);
+			insert.bindString(2,
+					"http://news.bbc.co.uk/rss/newsonline_world_edition/front_page/rss.xml");
+			insert.bindString(3, "BBC World");
+			insert.bindLong(4, 0);
+			insert.executeInsert();
+
+			insert.bindLong(1, 2);
+			insert.bindString(2, "http://rss.cnn.com/rss/edition.rss");
+			insert.bindString(3, "CNN Top Stories");
+			insert.bindLong(4, 0);
+			insert.executeInsert();
+
+			insert.bindLong(1, 2);
+			insert.bindString(2, "http://rss.news.yahoo.com/rss/topstories");
+			insert.bindString(3, "Yahoo News");
 			insert.bindLong(4, 0);
 			insert.executeInsert();
 
@@ -246,12 +228,6 @@ class DatabaseHelper extends SQLiteOpenHelper {
 			insert.bindString(2,
 					"http://rivals.yahoo.com/ncaa/football/blog/dr_saturday/rss.xml");
 			insert.bindString(3, "Dr. Saturday - NCAAF");
-			insert.bindLong(4, 0);
-			insert.executeInsert();
-
-			insert.bindLong(1, 3);
-			insert.bindString(2, "http://feeds.latimes.com/LAT_Sports_Blog");
-			insert.bindString(3, "LA Times Sports Blog");
 			insert.bindLong(4, 0);
 			insert.executeInsert();
 
@@ -282,7 +258,7 @@ class DatabaseHelper extends SQLiteOpenHelper {
 			insert.bindLong(1, 3);
 			insert.bindString(2,
 					"http://www.nytimes.com/services/xml/rss/nyt/Sports.xml");
-			insert.bindString(3, "NYTimes Sports ");
+			insert.bindString(3, "NYTimes Sports");
 			insert.bindLong(4, 0);
 			insert.executeInsert();
 
@@ -303,7 +279,7 @@ class DatabaseHelper extends SQLiteOpenHelper {
 			insert.bindString(2,
 					"http://feeds2.feedburner.com/wsj/xml/rss/3_7481.xml");
 			insert.bindString(3, "Wall Street Journal");
-			insert.bindLong(4, 1);
+			insert.bindLong(4, 0);
 			insert.executeInsert();
 
 			insert.bindLong(4, 2);
@@ -328,18 +304,6 @@ class DatabaseHelper extends SQLiteOpenHelper {
 			insert.executeInsert();
 
 			insert.bindLong(1, 5);
-			insert.bindString(2, "http://thinkprogress.org/feed/");
-			insert.bindString(3, "Think Progress");
-			insert.bindLong(4, 0);
-			insert.executeInsert();
-
-			insert.bindLong(1, 5);
-			insert.bindString(2, "http://politicalticker.blogs.cnn.com/feed/");
-			insert.bindString(3, "CNN Political Ticker");
-			insert.bindLong(4, 0);
-			insert.executeInsert();
-
-			insert.bindLong(1, 5);
 			insert.bindString(2, "http://www.mediaite.com/feed/");
 			insert.bindString(3, "Mediaite");
 			insert.bindLong(4, 0);
@@ -348,13 +312,6 @@ class DatabaseHelper extends SQLiteOpenHelper {
 			insert.bindLong(1, 5);
 			insert.bindString(2, "http://feeds.dailykos.com/dailykos/index.xml");
 			insert.bindString(3, "Daily Kos");
-			insert.bindLong(4, 0);
-			insert.executeInsert();
-
-			insert.bindLong(1, 5);
-			insert.bindString(2,
-					"http://abcnews.go.com/blogs/politics/the-note/feed/");
-			insert.bindString(3, "The Note");
 			insert.bindLong(4, 0);
 			insert.executeInsert();
 
@@ -376,14 +333,6 @@ class DatabaseHelper extends SQLiteOpenHelper {
 			insert.bindLong(1, 6);
 			insert.bindString(2, "http://www.topspeed.com/rss.xml");
 			insert.bindString(3, "Top Speed");
-			insert.bindLong(4, 0);
-			insert.executeInsert();
-
-			// 7 - Entertainment
-			insert.bindLong(1, 7);
-			insert.bindString(2,
-					"http://rss.cnn.com/rss/edition_entertainment.rss");
-			insert.bindString(3, "CNN Entertainment ");
 			insert.bindLong(4, 0);
 			insert.executeInsert();
 
@@ -453,12 +402,6 @@ class DatabaseHelper extends SQLiteOpenHelper {
 			insert.bindLong(1, 9);
 			insert.bindString(2, "http://feeds.feedburner.com/fashionistacom");
 			insert.bindString(3, "Fashionista");
-			insert.bindLong(4, 0);
-			insert.executeInsert();
-
-			insert.bindLong(1, 9);
-			insert.bindString(2, "http://thinkprogress.org/feed/");
-			insert.bindString(3, "Think Progress");
 			insert.bindLong(4, 0);
 			insert.executeInsert();
 
