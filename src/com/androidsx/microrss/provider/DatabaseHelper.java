e21e6b37154f28d1d1dd598a9a9d72b5181aca5e
Omar Pera <campbell.sx@gmail.com>
2011-09-12 18:30:41 +0000

    [microrss] db version and manifest

--- src/com/androidsx/microrss/provider/DatabaseHelper.java
+++ src/com/androidsx/microrss/provider/DatabaseHelper.java
@@ -17,7 +17,7 @@ import com.androidsx.microrss.provider.News.Items;
 class DatabaseHelper extends SQLiteOpenHelper {
     private static final String TAG = "DatabaseHelper";
     private static final String DATABASE_NAME = "microrss.db";
-    private static final int DATABASE_VERSION = 15;
+    private static final int DATABASE_VERSION = 3;
     
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
