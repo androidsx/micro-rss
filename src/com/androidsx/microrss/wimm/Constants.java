cf50a90fea060e0038d8914c8ecc87cf7b2e7f84
Pablo Pera <pablo.sx@gmail.com>
2012-01-22 21:31:02 +0000

    [microrss] Add some doc

--- src/com/androidsx/microrss/wimm/Constants.java
+++ src/com/androidsx/microrss/wimm/Constants.java
@@ -1,6 +1,11 @@
 package com.androidsx.microrss.wimm;
 
 public class Constants {
-    /** Are we running in the WIMM device, or on a standard Android device? */
+    /**
+     * Are we running in the WIMM device, or on a standard Android device?
+     * <p>
+     * Remember to add/remove the WIMM JAR file when you run against a standard Android device or
+     * the WIMM device.
+     */
     public static final boolean RUN_IN_WIMM_DEVICE = true;
 }
